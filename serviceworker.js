9cdd83050f03856721023909d5381fc9ccba4d72
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-21 09:47:56 +0000

    new stuff

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,28 @@
 var CACHE_NAME = 'gih-cache';
 var CACHED_URLS = [
   'offline.html',
-  'mystyles.css’,
+  'mystyles.css',
   'dino.png'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('install');
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('activate');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
-    console.log('Fetch request for:', event.request.url);
-    event.respondWith(new Response('header {background: green url("")!important}', {
-      headers: { 'Content-Type': 'text/css' }
-    }));
-  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
 });
-
