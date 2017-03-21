66f5982c60a77339c6941287892588274d86722a
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-21 09:41:09 +0000

    new stuff

--- serviceworker.js
+++ serviceworker.js
@@ -1,31 +1,3 @@
-var CACHE_NAME = 'gih-cache';
-var CACHED_URLS = [
-  'offline.html',
-  'mystyles.css',
-  'dino.png'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
-        }
-      });
-    })
-  );
-});
 self.addEventListener('install', function(event) {
   console.log('install');
 });
