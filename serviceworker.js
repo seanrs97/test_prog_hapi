a51914dbea86c202864c1864f6d0fd012487f0ef
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-21 09:37:32 +0000

    new stuff

--- serviceworker.js
+++ serviceworker.js
@@ -26,3 +26,20 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+self.addEventListener('install', function(event) {
+  console.log('install');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activate');
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
+    console.log('Fetch request for:', event.request.url);
+    event.respondWith(new Response('header {background: green url("")!important}', {
+      headers: { 'Content-Type': 'text/css' }
+    }));
+  }
+});
+
